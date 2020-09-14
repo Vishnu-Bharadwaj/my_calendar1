63400ed23a4947dc7812217a87e9bef014a3bc71
Vishnu-Bharadwaj <70241544+Vishnu-Bharadwaj@users.noreply.github.com>
2020-09-13 21:34:19 -0700

    Add files via upload

---
+++ mycalendar1.py
@@ -0,0 +1,38 @@
+# mycalendar1.py
+# - a calendar of my school schedule on Mondays and Tuesdays
+
+day = 'Day'
+no_day = ''
+time = 'Time'
+subject = 'Subject'
+one_tab ='\t'
+two_tabs = '\t\t'
+print(day+two_tabs+time+two_tabs+one_tab+subject)
+
+day = 'Monday'
+time = '9:10 - 10:15'
+subject = 'Language Arts'
+print(day+two_tabs+time+two_tabs+subject)
+
+time = '10:35 - 11:40'
+subject = 'Social Studies'
+print(no_day+two_tabs+time+two_tabs+subject)
+
+time = '12:10 - 1:15'
+subject = 'Science'
+print(no_day+two_tabs+time+two_tabs+subject)
+
+day = 'Tuesday'
+time = '9:10 - 10:15'
+subject = 'Math'
+print(day+two_tabs+time+two_tabs+subject)
+
+time = '10:35 - 11:40'
+subject = 'Exploratory Wheel'
+print(no_day+two_tabs+time+two_tabs+subject)
+
+time = '12:10 - 1:15'
+subject = 'Physical Fitness'
+print(no_day+two_tabs+time+two_tabs+subject)
+
+
